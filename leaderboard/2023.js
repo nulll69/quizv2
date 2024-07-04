d9f56c55e46274a7dae7d83b1851363e12c200ce
nulll69 <junsarakal@gmail.com>
2024-07-04 21:25:52 +0800

    leaderboard from 2023. From July 15 to December 31

---
+++ leaderboard/2023.js
@@ -0,0 +1,26 @@
+const { rankv1 } = require("../db");
+async function leaderboardv1(page) {
+  try {
+   const players = await rankv1();
+   const size = 5;
+   const start = (page - 1) * size;
+    const end = start + size;
+ if (start >= players.length) {
+  return { msg: `There are only ${Math.ceil(players.length / size)} total pages.` };
+  }
+ const pagePlayers = players.slice(start, end);
+ let msg = `Top Players from 2023\n\n`;
+ pagePlayers.forEach((player, i) => {
+  const rank = start + i + 1;
+ msg += `${rank}. Id: ${player.playerid},\n`;
+ msg += `Correct answers: ${player.correct}\n`;
+  msg += `Wrong answers: ${player.wrong}\n\n`;
+  });
+ msg += `Total Players: ${players.length}\nPages: ${pg}/${Math.ceil(players.length / size)}\nType {p} leaderboard <page num> to view the next page`;
+   return { msg: msg.trim() };
+  } catch (error) {
+ console.error("Error fetching leaderboard:", error);
+ return { msg: "Error fetching leaderboard." };
+    }
+}
+module.exports = { leaderboardv1 };
